d6c5edf11f3b07828f1567169aa08204ad52ec94
Ayush Tripathi <ayushtripathi9821@gmail.com>
2026-04-12 23:32:30 +0530

    Added : Bubble Sort

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,11 @@
+data = input("Enter numbers separated by space: ")
+arr = [int(x) for x in data.split()]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
